4b3a376fbf7c6bdfc5ce8eb4c826f063af8dba73
AngaBlue <contact@anga.blue>
2024-06-21 00:36:46 +1000

    test: custom error handler

---
+++ test/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import validate from '../src/index';
+import { z } from 'zod';
+
+const app = express();
+const port = 3000;
+
+app.get('/', validate({
+    handler: (errors, _req, res) => {
+        console.error(errors);
+        return res.status(400).send('Validation failed');
+    },
+    query: {
+        name: z.string().min(3).max(10),
+        age: z.coerce.number().min(18),
+    },
+}), (req, res) => {
+    const { name, age } = req.query;
+    res.send(`Hello ${name}! (Your age is ${age})`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
