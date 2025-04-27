c6a2e375ff7baa9c971552987aa691b2a6eaf7ec
AngaBlue <contact@anga.blue>
2025-04-27 14:12:28 +1000

    build: cjs + esm export

--- test/index.ts
+++ test/index.ts
@@ -1,22 +1,39 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
 import { z } from 'zod';
-import validate from '../src/index';
+import validate, { type WeakRequestHandler } from '../src/index';
 
 const app = express();
 const port = 3000;
 
+const authenticate = (_req: Request, _res: Response, next: NextFunction) => {
+	next();
+};
+
+const query = {
+	name: z.string().min(3).max(10),
+	age: z.coerce.number().min(18)
+};
+
+const body = {
+	title: z.string().max(4)
+};
+
+const params = {
+	id: z.coerce.number()
+};
+
 app.get(
-	'/',
+	'/:id',
+	authenticate as WeakRequestHandler,
 	validate({
 		handler: (errors, _req, res) => {
 			console.error(errors);
 			res.status(400).send('Validation failed');
 			return;
 		},
-		query: {
-			name: z.string().min(3).max(10),
-			age: z.coerce.number().min(18)
-		}
+		body,
+		query,
+		params
 	}),
 	(req, res) => {
 		const { name, age } = req.query;
