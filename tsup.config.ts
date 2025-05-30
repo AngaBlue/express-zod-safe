8639312e9354935754e49fe4f523098b6ab5d975
AngaBlue <contact@anga.blue>
2025-05-30 11:37:50 +1000

    fix: cjs interop

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
-  clean: true,
-  sourcemap: true,
+	entry: ['src/index.ts'],
+	format: ['esm', 'cjs'],
+	dts: true,
+	clean: true,
+	sourcemap: true,
+	cjsInterop: true,
+	splitting: true
 });
